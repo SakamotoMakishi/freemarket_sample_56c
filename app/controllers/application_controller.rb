54bf8e713233818c3a652e18d9e52673be3320bc
SakamotoMakishi <dauw.mf81@gmail.com>
2019-08-05 12:59:16 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
+
+  private
+
+  def production?
+    Rails.env.production?
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["freemarket_sample_56c"] && password == ENV["teamcywts56"]
+    end
+  end
 end
