a2c0c88b44c406d815ab4c5e92e08d581fa6b24e
t-tatsuhara <adgjmptw19890305@icloud.com>
2019-08-07 18:51:29 +0900

    newActiveStorage

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,12 @@
 class ItemsController < ApplicationController
 
   def index
+    # @women＿items = Category.find(1).items
+    # @men＿items = Category.find(2).items
+    # @child＿items = Category.find(3).items
+    # @chanel＿items = Brand.find(1).items
+    # @mvuitton＿items = Brand.find(2).items
+    # @nike＿items = Brand.find(3).items
   end
 
   def new
