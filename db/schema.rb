bffd4b3e531090ec2166f1c12328ceb6683342a1
SakamotoMakishi <dauw.mf81@gmail.com>
2019-08-12 16:55:43 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_10_142813) do
+ActiveRecord::Schema.define(version: 2019_08_12_075302) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 2019_08_10_142813) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "card_id"
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -91,7 +90,6 @@ ActiveRecord::Schema.define(version: 2019_08_10_142813) do
     t.datetime "updated_at", null: false
     t.bigint "category_id"
     t.bigint "brand_id"
-    t.string "seller_name"
     t.index ["brand_id"], name: "index_items_on_brand_id"
     t.index ["category_id"], name: "index_items_on_category_id"
     t.index ["name"], name: "index_items_on_name"
