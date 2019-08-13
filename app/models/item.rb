f126123e188d6c3f8bd554d3005ef84d2aad67ed
SakamotoMakishi <dauw.mf81@gmail.com>
2019-08-13 21:31:44 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,6 @@ class Item < ApplicationRecord
   validates :name, :text, :status, :price, presence: true
   # belongs_to :user
   belongs_to :seller, class_name: "User"
-  belongs_to :category, optional:true
-  belongs_to :brand, optional:true
   has_one :delivery
 
   has_many_attached :images
