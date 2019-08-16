41783b1c2d4acffab20b22e66f5795d34420ab2b
t-tatsuhara <adgjmptw19890305@icloud.com>
2019-08-16 17:17:21 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,6 @@
 class Item < ApplicationRecord
   validates :name, :text, :status, :price, presence: true
   belongs_to :seller, class_name: "User"
-  belongs_to :category, optional:true
   belongs_to :brand, optional:true
   has_one :delivary
 
