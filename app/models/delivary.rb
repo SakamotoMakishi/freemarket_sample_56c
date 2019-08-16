f0694e77a0bf6a77eab48581fbe1ce7c14b25896
SakamotoMakishi <dauw.mf81@gmail.com>
2019-08-17 02:50:29 +0900

    削除機能修正

--- app/models/delivary.rb
+++ app/models/delivary.rb
@@ -1,4 +1,4 @@
 class Delivary < ApplicationRecord
   validates :price, :area, :delivary_day, :delivary_method, presence: true
-  belongs_to :item
+  belongs_to :item,optional: true
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ApplicationRecord
   validates :name, :text, :status, :price, presence: true
   belongs_to :seller, class_name: "User"
   has_many_attached :images
+  has_one :delivary, foreign_key: :item_id, dependent: :destroy
 end
