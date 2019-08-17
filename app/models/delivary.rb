8fc3270f0a7b610b86e24915b76069ea26ab6f71
t-tatsuhara <adgjmptw19890305@icloud.com>
2019-08-17 11:48:45 +0900

    addErrorMessageNewItem

--- app/models/delivary.rb
+++ app/models/delivary.rb
@@ -1,4 +1,16 @@
 class Delivary < ApplicationRecord
-  validates :price, :area, :delivary_day, presence: true
   has_one :item
+  validate :add_error_message
+
+  def add_error_message
+    if price.blank? || price.nil?
+      errors[:price] << "入力してください"
+    end
+    if area.blank? || area.nil?
+      errors[:area] << "入力してください"
+    end
+    if delivary_day.blank? || delivary.nil?
+      errors[:delivary_day] << "選択してください"
+    end
+  end
 end
