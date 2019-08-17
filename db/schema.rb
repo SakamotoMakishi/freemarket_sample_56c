2caeecb1a96290affec6b47995626006bad436e4
t-tatsuhara <adgjmptw19890305@icloud.com>
2019-08-17 12:26:06 +0900

    updateErrorMessageNewItem

--- db/schema.rb
+++ db/schema.rb
@@ -46,7 +46,6 @@ ActiveRecord::Schema.define(version: 2019_08_16_113528) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "phone_num"
   end
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -73,9 +72,9 @@ ActiveRecord::Schema.define(version: 2019_08_16_113528) do
   create_table "delivaries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "area", null: false
     t.string "delivary_day", null: false
+    t.integer "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "item_id"
     t.string "price", null: false
     t.string "delivary_method"
   end
@@ -111,3 +110,4 @@ ActiveRecord::Schema.define(version: 2019_08_16_113528) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
+
