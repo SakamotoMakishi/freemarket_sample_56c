8f3d44b459225f9c7dfeb75fb874baf7d2b75e2e
naomotchi <mukorobinanaoki@gmail.com>
2019-08-26 17:38:14 +0900

    やることリスト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show, :card_add_to]
   before_action :set_header
-  before_action :set_item, only: [:listing, :trading, :completed, :purchase, :purchased, :show]
+  before_action :set_item, only: [:show, :listing, :trading, :completed, :purchase, :purchased, :things]
   before_action :set_card, only: [:card_add_to]
 
   def show
@@ -12,6 +12,9 @@ class UsersController < ApplicationController
   end
 
   def card_add_to
+  end
+
+  def things
   end
 
   def listing
@@ -34,7 +37,8 @@ class UsersController < ApplicationController
   def set_item
     @item_seller = Item.with_attached_images.where(seller_id: current_user.id).where(buyer_id: nil).order("id DESC").limit(10)
     @item_buyer = Item.with_attached_images.where(buyer_id: current_user.id).order("id DESC").limit(5)
-    @completed =  Item.with_attached_images.where("buyer_id > ?", 1).where(seller_id: current_user.id).order("id DESC").limit(10)
+    @completed =  Item.with_attached_images.where(seller_id: current_user.id).where("buyer_id > ?", 1).order("id DESC").limit(10)
+    @user = User.where(id: @completed.map{|hash| hash[:buyer_id]})
   end
 
   def set_card
